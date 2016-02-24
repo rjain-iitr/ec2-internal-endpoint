b85f80ba7dd3816d6b28af01117e1695b9e72b9e
root <root@sbscinder2.sbs.staging.jiocloudservices.com>
2016-02-24 16:08:29 +0530

    ci changes

--- volumes/describe.sh
+++ volumes/describe.sh
@@ -2,4 +2,4 @@ user_var=$UserId
 project_id=$ProjectId
 token=$Token
 detail=False
-curl -X GET -H "Accept-Encoding: identity" -H "User-Agent: curl/7.35.0" -H "Content-Type: application/json" "http://10.140.214.135:8788/services/Cloud/?TokenId=$token&ProjectId=$project_id&UserId=$user_var&RequestId=req-c0ac5d63-78&Action=DescribeVolumes&Detail=$detail"
+curl -X GET -H "Accept-Encoding: identity" -H "User-Agent: curl/7.35.0" -H "Content-Type: application/json" "https://sbs.ind-west-1.staging.jiocloudservices.com/services/Cloud/?TokenId=$token&ProjectId=$project_id&UserId=$user_var&RequestId=req-c0ac5d63-78&Action=DescribeVolumes&Detail=$detail"
